593af8ef7440518d22da2b50b462a9779f072fa9
ayeshanadeem-PF <ifra51746@gmail.com>
2024-10-28 18:44:19 +0500

    my first task on git hub

---
+++ task 1.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int num1=20;
+
+int num2=10;
+
+int sum = num1+num2;
+
+cout<<sum<<endl;
+
+int sub= num1-num2;
+
+cout<<sub<<endl;
+
+int mul= num1*num2;
+
+cout<<mul<<endl;
+
+float decimal= num1/num2;
+
+cout<<decimal<<endl;
+
+int modulus= num1%num2;
+
+cout<<modulus<<endl;
+return 0;
+}
